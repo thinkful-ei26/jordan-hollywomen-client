6ba84db1c82c99d2f1b2b853f4bd0e84892aa09d
Jordan <jordanhaddadi@gmail.com>
2019-01-11 18:02:27 -0500

    spaced the footer, deploying to heroku

--- src/components/app-info.js
+++ src/components/app-info.js
@@ -13,7 +13,7 @@ export class AppInfo extends Component {
                         <p> If you've ever searched on IMDB, you've undoubtedly noticed that Hollywood tends to heavily favor male leads over females. 
                             Netflix and other streaming sites have even created categories like 'Strong Female Lead' 
                             because women with starring roles in Hollywood is such a rarity.</p>
-                    </div>
+                    </div><br/><br/><br/><br/>
                     <div className="how-to">  
                         <footer>Enter a search term and select either TV or Movie from the drop down menu to get the posters and titles from everything that matches. All you have to do next is click on the poster!</footer>
                     </div>
